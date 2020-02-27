de167e17a8a7292877b8d64ef9004cdd9a29a89a
tamas.lorincz <tamas.lorincz@ginf.systems>
2020-02-27 12:03:42 +0100

    initial

---
+++ main.c
@@ -0,0 +1,6 @@
+#include<stdio.h>	//Pre-processor directive
+void main()		//main function declaration
+{
+printf("Hello World");	//to output the string on a display
+getch ();		//terminating function
+}
