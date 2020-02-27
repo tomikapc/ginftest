8a7f1aa098a23b19554fde8af12a247abf15bdf2
tamas.lorincz <tamas.lorincz@ginf.systems>
2020-02-27 12:20:11 +0100

    CMake

--- main.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>	//Pre-processor directive
-void main()		//main function declaration
-{
-printf("Hello World");	//to output the string on a display
-getch ();		//terminating function
-}
